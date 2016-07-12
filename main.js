47a74847f6aade4004e6dfdc40a946def6b5d991
asejdina <asejdina@gmail.com>
2016-07-12 11:00:24 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,28 @@
+(function(){
+  $(document).ready(init);
+
+  var clickCount = 0;
+
+  function init(){
+    $('.cell').on('click', placeTic);
+  }
+
+  function placeTic(){
+    clickCount++;
+
+    if(clickCount % 2 === 0){
+      $(this).css({
+        "background-image": "url("+'./ironman.png'+")",
+        "background-color": "red"
+      });
+    } else {
+      $(this).css({
+        "background-image": "url("+'./cap.png'+")",
+        "background-color": "blue"
+      });
+    }
+  }
+
+
+
+})();
