c6073742ee89c639bd56c349737628e58abf87a1
asejdina <asejdina@gmail.com>
2016-07-18 16:05:36 -0400

    made it so style is added via css instead of javascript

--- main.js
+++ main.js
@@ -11,16 +11,8 @@
     clickCount++;
 
     if(clickCount % 2 === 0){
-      $(this).css({
-        "background-image": "url("+'./ironman.png'+")",
-        "background-color": "red"
-      });
       $(this).addClass('red');
     } else {
-      $(this).css({
-        "background-image": "url("+'./cap.png'+")",
-        "background-color": "blue"
-      });
       $(this).addClass('blue');
     }
   }
